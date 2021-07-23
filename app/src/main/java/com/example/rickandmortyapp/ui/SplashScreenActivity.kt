318b226b677e41943efe09c88b7f8c47a6576825
Alex <a_v_ilinskiy@mail.ru>
2021-07-23 15:30:45 +0300

    Correction of remarks

--- app/src/main/java/com/example/rickandmortyapp/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/example/rickandmortyapp/ui/SplashScreenActivity.kt
@@ -7,18 +7,17 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.rickandmortyapp.MainActivity
 import com.example.rickandmortyapp.R
 
+const val SPLASH_DISPLAY_LENGHT = 3000L
 
 class SplashScreenActivity : AppCompatActivity() {
-
-    private val SPLASH_DISPLAY_LENGHT = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
         Handler().postDelayed(Runnable {
-            val mainIntent = Intent(this@SplashScreenActivity, MainActivity::class.java)
-            this@SplashScreenActivity.startActivity(mainIntent)
-            this@SplashScreenActivity.finish()
-        }, SPLASH_DISPLAY_LENGHT.toLong())
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, SPLASH_DISPLAY_LENGHT)
     }
 }
