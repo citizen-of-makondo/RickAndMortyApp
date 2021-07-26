bf6ab40cf4c4f45d538481faa0d0db07083f3036
Alex <a_v_ilinskiy@mail.ru>
2021-07-26 17:19:41 +0300

    Made a back button on search

--- app/src/main/java/com/example/rickandmortyapp/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyapp/MainActivity.kt
@@ -1,16 +1,47 @@
 package com.example.rickandmortyapp
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.example.rickandmortyapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+
+        val manager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val search = menu?.findItem(R.id.search)
+        val searchView = search?.actionView as SearchView
+
+        searchView.setSearchableInfo(manager.getSearchableInfo(componentName))
+
+        searchView.queryHint = "Search"
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchView.clearFocus()
+                searchView.setQuery("", false)
+                search.collapseActionView()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+        return true
     }
 }
